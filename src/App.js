a918e698511bfcb4206a0fca00db899067431dea
W0jti <wiechecwojciech@gmail.com>
2024-05-18 11:11:57 +0200

    First commit React. Changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-
-    let email = 'wiechecwojciech@gmail.com';
+    const [email, setEmail] = useState("wiechecwojciech@gmail.com");
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
+    }
+
+    let message = <div>Ale masz krótki adres!</div>
+    if(email.length > 25) {
+        message = <div>Twój email jest za długi!!</div>
+    } else if (email.length > 10) {
+        message = <div>Twój email jest w sam raz</div>
     }
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email}</h2>
-            <input type="text" onChange={handleChange}/>
+            {message}
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
